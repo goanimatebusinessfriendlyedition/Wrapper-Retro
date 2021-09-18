7c3f6f75694e1c4e7e321a7fc9fbdb8bee90eff1
goanimatebusinessfriendlyedition <84991743+goanimatebusinessfriendlyedition@users.noreply.github.com>
2021-09-18 01:20:54 -0400

    Add files via upload

--- wrapper/theme/list.js
+++ wrapper/theme/list.js
@@ -36,7 +36,7 @@
 const fUtil = require('../fileUtil');
 const folder = process.env.THEME_FOLDER;
 module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/goapi/getThemeList/') return;
+	if (req.method != 'POST' || url.path != '/goapi/getThemeList/?') return;
 	res.setHeader('Content-Type', 'application/zip');
 	fUtil.zippy(`${folder}/themelist.xml`, 'themelist.xml').then(b => res.end(b));
 	return true;
